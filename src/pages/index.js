bbaee2b75a142d3227a2b9f8026bb1370883b47a
Lương Quang Mạnh <luongquangmanh85@gmail.com>
2020-03-20 23:32:24 +0700

    Complete side bar layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,11 +6,11 @@ import { ResponseDivision } from '../components/divisions/response'
 const Index = () => (
   <Layout left={<SideBar />}>
     <div className="grid grid-cols-3 h-full">
-      <div className="col-span-1 border-l border-r border-gray-500 p-4">
+      <div className="col-span-1 border-r border-gray-400 p-4">
         <MibBrowserDivision />
       </div>
       <div className="col-span-2 flex flex-col">
-        <div className="border-b border-gray-500 p-4">
+        <div className="border-b border-gray-400 p-4">
           <RequestDivision />
         </div>
         <div className="p-4">
