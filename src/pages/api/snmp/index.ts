038170e6ac4a3f11c69dd727721fcb5e135e9a45
Lương Quang Mạnh <luongquangmanh85@gmail.com>
2020-04-08 00:40:16 +0700

    Complete POST /api/snmp

--- src/pages/api/snmp/index.ts
+++ src/pages/api/snmp/index.ts
@@ -1,5 +1,20 @@
+import { SnmpClient } from '@lqmanh/boracium'
 import { NextApiRequest, NextApiResponse } from 'next'
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ iam: '/api/snmp' })
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== 'POST') return res.status(204).json({})
+
+  const { host, port, version, community, oid, method } = req.body
+
+  // no support for SNMP v3 yet
+  if (version === 'v3') return res.json({ msg: 'SNMP_V3_NOT_SUPPORTED' })
+
+  const client = new SnmpClient({ host, port, version, community })
+
+  let varbinds
+  if (method === 'GET') varbinds = await client.get(oid)
+  else if (method === 'GETNEXT') varbinds = await client.getNext(oid)
+  else if (method === 'GETBULK') varbinds = await client.getBulk(oid)
+  else return res.json({ msg: 'SNMP_METHOD_UNKNOWN' })
+  return res.json(varbinds)
 }
