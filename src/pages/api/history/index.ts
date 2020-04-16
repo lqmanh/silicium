26db7bac396c079f1fdece1ea4ef555676b0dffc
Lương Quang Mạnh <luongquangmanh85@gmail.com>
2020-04-17 00:52:07 +0700

    Fix GET, POST /api/history

--- src/pages/api/history/index.ts
+++ src/pages/api/history/index.ts
@@ -13,14 +13,14 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     if (req.method === 'GET') {
       const snapshot = await collection.get()
-      const entries = snapshot.docs
+      const entries = snapshot.docs.map((doc) => doc.data())
 
       return res.status(200).json(entries)
     } else if (req.method === 'POST') {
       const { request, response } = req.body
       await collection.add({ request, response })
 
-      return res.status(204).json({})
+      return res.status(201).json({})
     } else return res.status(405).json({})
   } catch (err) {
     return res.status(500).json({ msg: err.message })
