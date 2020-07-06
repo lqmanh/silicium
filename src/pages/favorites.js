224aeb3b3903ec207112991c072e35f771c67bbf
Lương Quang Mạnh <luongquangmanh85@gmail.com>
2020-07-07 02:35:48 +0700

    Update things

--- src/pages/favorites.js
+++ src/pages/favorites.js
@@ -1,6 +1,8 @@
 import { observer } from 'mobx-react'
 import Head from 'next/head'
 import { useEffect, useState } from 'react'
+import { Icon, IconText } from '../components/common/elements'
+import { Button } from '../components/common/form/controls'
 import { DivisionTitle, TabTitle } from '../components/common/typography/titles'
 import { FavoriteAgentCard } from '../components/favorites'
 import { Tab, TabBar } from '../components/layout'
@@ -9,14 +11,24 @@ import { useStores } from '../hooks'
 const Favorites = observer(() => {
   const tabs = ['Favorite Agents']
   const [activeTab, setActiveTab] = useState(tabs[0])
+  const { favoritesStore: favStore } = useStores()
   const title = <DivisionTitle>favorites</DivisionTitle>
+  const clear = async (event) => {
+    event.preventDefault()
+    await favStore.clear()
+  }
+  const clearButton = (
+    <Button bgColor="hover:bg-red-600" textColor="text-red-600 hover:text-white" onClick={clear} key="clear-button">
+      <IconText icon={<Icon name="trash-outline" />} text="Clear" />
+    </Button>
+  )
 
   return (
     <div>
       <Head>
         <title>Favorites | Silicium</title>
       </Head>
-      <TabBar title={title} actions={[]}>
+      <TabBar title={title} actions={[clearButton]}>
         {tabs.map((tab, i) => (
           <Tab active={tab === activeTab} onClick={() => setActiveTab(tab)} key={i}>
             <TabTitle>{tab}</TabTitle>
